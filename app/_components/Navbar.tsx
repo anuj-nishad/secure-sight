0da6eb84000df04c73906ffb5aa4c0446ea6823d
anuj-nishad <nishadanuj995@gmail.com>
2025-07-22 17:11:52 +0530

    bug-fix

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -3,15 +3,16 @@
 import Image from "next/image";
 import { BellDot, CircleUserRound} from "lucide-react"
 import NavbarRoutes from "./navbarRoutes";
+import Link from "next/link";
 
 const Navbar = () => {
 
   return (
   <div className="h-full p-4 border-b flex items-center justify-around shadow-sm">
-    <div className="flex items-center gap-x-2">
+    <Link className="flex items-center gap-x-2" href={"/"}>
       <Image src="/icons/logo.svg" alt="desc" width={35} height={35}/>
       <h1 className="text-xl">SecureSight</h1>
-    </div>
+    </Link>
     <NavbarRoutes/>
     <div className="flex items-center gap-x-6">
       <BellDot/>
