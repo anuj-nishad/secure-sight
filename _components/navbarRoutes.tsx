af16f138d3b343c7f27e6dd00251bd214376e3a4
anuj-nishad <nishadanuj995@gmail.com>
2025-07-22 15:29:49 +0530

    Navbar

---
+++ _components/navbarRoutes.tsx
@@ -0,0 +1,45 @@
+"use client"
+import {Aperture, Cctv, LayoutDashboard,ShieldAlert,Users } from "lucide-react"
+import NavBarItem from "./navbarItem";
+const routes = [
+  {
+    icon: LayoutDashboard,
+    label: "Dashboard",
+    href: "/",
+  },
+  {
+    icon: Cctv,
+    label: "Cameras",
+    href: "/cameras",
+  },
+  {
+    icon: Aperture,
+    label: "Scenes",
+    href: "/scenes",
+  },
+  {
+    icon: ShieldAlert,
+    label: "Incidents",
+    href: "/incidents",
+  },
+  {
+    icon: Users,
+    label: "Users",
+    href: "/users",
+  }
+]
+
+const NavbarRoutes = () => {
+  return ( <div className="flex gap-x-8">
+    {routes.map((route)=>(
+        <NavBarItem
+          key={route.href}
+          icon={route.icon}
+          label={route.label}
+          href={route.href}
+        />
+      ))}
+  </div> );
+}
+ 
+export default NavbarRoutes;
